4908f69d402121273a0e23fe95db3ad44e5f4768
Kazimierz Filip <kazimierzfilipmail@gmail.com>
2020-11-19 11:38:55 +0000

    configuration file

---
+++ configuration.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+test=`ping -c 2 8.8.8.8 2>&1`
+if [[ $test == "connect: Network is unreachable" ]]; then
+   echo "$test"
+   exit 1
+fi
+
+echo 'Update apt'
+
+apt-get update
+
+echo 'install git'
+
+apt-get -y install git
+
+echo 'install python3'
+
+apt-get -y install python3
+
+echo 'install curl'
+
+apt-get -y install curl
+
+echo 'install pip'
+
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+
+echo 'install unicorn'
+
+python3 -m pip install unicorn
+
+echo 'download backup'
+
+git config --global user.name "Kazimierz Filip"
+git config --global user.email "kazimierzfilipmail@gmail.com"
+
+git clone https://github.com/kazimierzfilip/test.git
