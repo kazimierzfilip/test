46fed97637e95739b4aea2275feee14ae5c718fc
Kazimierz Filip <kazimierzfilipmail@gmail.com>
2020-12-10 15:37:42 +0100

    added exaple

--- qilingdemo.py
+++ qilingdemo.py
@@ -1,16 +1,24 @@
+from unicorn import *
+from capstone import *
+
+import sys
+sys.path.append("..")
 from qiling import *
 
-root = "./"
-path = "./unicorn/bubble.elf"
+md = Cs(CS_ARCH_X86, CS_MODE_64)
+stepCount = 0
 
 
 def print_asm(ql, address, size):
-	buf = ql.mem.read(address, size)
-	for i in md.disasm(buf, address):
-		print(":: 0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
+    global stepCount
+    stepCount+=1
+    buf = ql.mem.read(address, size)
+    for i in md.disasm(buf, address):
+        print(":: 0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
 
 
 if __name__ == "__main__":
-	ql = Qiling(["./unicorn/bubble.elf"], "./unicorn")
-	ql.hook_code(print_asm)
-	ql.run()
+    ql = Qiling(["rootfs/bin/bubble.elf"], "rootfs")
+    ql.hook_code(print_asm)
+    ql.run()
+    print('Steps: ', stepCount)
