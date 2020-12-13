dbad04bd9ac982d67695f867b0553c488cbcfc4b
Kazimierz Filip <kazimierzfilipmail@gmail.com>
2020-12-13 15:23:14 +0000

    test loops

--- qilingdemo.py
+++ qilingdemo.py
@@ -7,14 +7,21 @@ from qiling import *
 
 md = Cs(CS_ARCH_X86, CS_MODE_64)
 stepCount = 0
+blockCount = 0
 
 
 def print_asm(ql, address, size):
-    buf = ql.mem.read(address, size)
-    for i in md.disasm(buf, address):
-        #print(":: 0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
-        global stepCount
-        stepCount+=1
+    global stepCount
+    stepCount+=1
+    #buf = ql.mem.read(address, size)
+    #for i in md.disasm(buf, address):
+    #    print(":: 0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
+
+
+def ql_hook_block_disasm(ql, address, size):
+    global blockCount
+    blockCount+=1
+    ql.nprint("\n[+] Tracing basic block at 0x%x" % (address))
 
 
 if __name__ == "__main__":
@@ -22,13 +29,32 @@ if __name__ == "__main__":
     ql.hook_code(print_asm)
     ql.run()
     print('Steps: ', stepCount)
+
     stepCount = 0
     ql = Qiling(["rootfs/bin/bubble2.elf"], "rootfs")
     ql.hook_code(print_asm)
     ql.run()
     print('Steps: ', stepCount)
+
     stepCount = 0
     ql = Qiling(["rootfs/bin/bubble3.elf"], "rootfs")
     ql.hook_code(print_asm)
     ql.run()
     print('Steps: ', stepCount)
+
+    stepCount = 0
+    ql = Qiling(["rootfs/bin/testloop1.elf"], "rootfs")
+    ql.hook_code(print_asm)
+    ql.hook_block(ql_hook_block_disasm)
+    ql.run()
+    print('Steps: ', stepCount)
+    print('Blocks: ', blockCount)
+
+    stepCount = 0
+    blockCount = 0
+    ql = Qiling(["rootfs/bin/testloop2.elf"], "rootfs")
+    ql.hook_code(print_asm)
+    ql.hook_block(ql_hook_block_disasm)
+    ql.run()
+    print('Steps: ', stepCount)
+    print('Blocks: ', blockCount)
---
+++ unicorn/testloop.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+int main()  
+{  
+    int i;  
+    for (i = 0; i < 1000000000000000; i++)  
+        cout << i << " ";  
+    cout << endl;
+    return 0;  
+}  
