efb9c4a7e9d3c8420df584f221a5ca8948b6cc78
Kazimierz Filip <kazimierzfilipmail@gmail.com>
2020-12-17 11:53:11 +0000

    tests on hooks

--- qilingdemo.py
+++ qilingdemo.py
@@ -5,24 +5,27 @@ import sys
 sys.path.append("..")
 from qiling import *
 
-md = Cs(CS_ARCH_X86, CS_MODE_64)
+md = Cs(CS_ARCH_X86, CS_MODE_32)
 stepCount = 0
+stepCount2 = 0
 blockCount = 0
 
 
 def print_asm(ql, address, size):
     global stepCount
     stepCount+=1
-    ql.nprint("\npc= ",ql.reg.arch_pc)
-    #buf = ql.mem.read(address, size)
-    #for i in md.disasm(buf, address):
-    #    print(":: 0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
+    #print("pc= ",ql.reg.arch_pc)
+    buf = ql.mem.read(address, size)
+    for i in md.disasm(buf, address):
+        global stepCount2
+        stepCount2+=1
+        #print(":: 0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
 
 
 def ql_hook_block_disasm(ql, address, size):
     global blockCount
     blockCount+=1
-    ql.nprint("\n[+] Tracing basic block at 0x%x" % (address))
+    #print("[+] Tracing basic block at 0x%x" % (address))
 
 
 if __name__ == "__main__":
@@ -63,23 +66,19 @@ if __name__ == "__main__":
 #    print('Blocks: ', blockCount)
 
     stepCount = 0
-    blockCount = 0
-    ql = Qiling(["rootfs/bin/testloop3.elf"], "rootfs")
+    ql = Qiling(["rootfs/x86_linux/bin/testloopCoutNotOptDouble.elf"], "rootfs/x86_linux")
     ql.hook_code(print_asm)
-    ql.hook_block(ql_hook_block_disasm)
-    print("\npc= ",ql.reg.arch_pc)
     ql.run()
-    print("\npc= ",ql.reg.arch_pc)
-    print('loop 3 Steps: ', stepCount)
-    print('Blocks: ', blockCount)
+    print('loop Steps: ', stepCount)
 
     stepCount = 0
-    blockCount = 0
-    ql = Qiling(["rootfs/bin/testloop4.elf"], "rootfs")
+    ql = Qiling(["rootfs/x86_linux/bin/testloopCoutNotOptDouble2.elf"], "rootfs/x86_linux")
     ql.hook_code(print_asm)
-    ql.hook_block(ql_hook_block_disasm)
-    print("\npc= ",ql.reg.arch_pc)
     ql.run()
-    print("\npc= ",ql.reg.arch_pc)
     print('Steps: ', stepCount)
-    print('Blocks: ', blockCount)
+
+    stepCount = 0
+    ql = Qiling(["rootfs/x86_linux/bin/testloopCoutNotOptDouble3.elf"], "rootfs/x86_linux")
+    ql.hook_code(print_asm)
+    ql.run()
+    print('Steps: ', stepCount)
--- unicorn/testloop.cpp
+++ unicorn/testloop.cpp
@@ -3,9 +3,16 @@ using namespace std;
   
 int main()  
 {  
-    int i;
+    int i,j;
     int sum = 0;
-    for (i = 0; i < 1000; i++)  
+    for (i = 0; i < 100; i++){
+       int sum2 = 0;
+       for (j = 0; j < 100; j++) {
+        sum2 += 1;
+       }
         sum += 1;
+        cout << sum2;
+    }
+    cout << sum;
     return 0;  
 }  
