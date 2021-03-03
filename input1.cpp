55c84c8c234419e07314a25349cb14068cbd9bb3
Kazimierz Filip <kazimierzfilipmail@gmail.com>
2021-03-03 17:02:05 +0000

    testing input

---
+++ input1.cpp
@@ -0,0 +1,14 @@
+#include<iostream> 
+using namespace std; 
+  
+int main()  
+{  
+      
+    cout<<"Hello"<<endl; 
+
+    char test;
+    cin>>test;
+    cout<<"test"<<test<<endl;
+
+    return 0;  
+}  
